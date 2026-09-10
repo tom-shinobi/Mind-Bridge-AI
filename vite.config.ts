12bcc2c68ca646bba7c1f5efc8ef23912b059819
J Sanjay Aron <tom-shinobi@users.noreply.github.com>
2026-09-10 22:21:33 +0530

    feat(network): enable allowedHosts in vite.config.ts for public tunneling and remote access

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: true
+  }
 })
 
